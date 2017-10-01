1e6de6ef9c053fdaeab1117407ea0fb615f543f3
nulpatrol <rostyslav.khanyukov@ms.sumdu.edu.ua>
2017-10-01 14:40:55 +0300

    feat

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const lib = require('./lib/file');
 const dias = require('./lib/diasHelper');
 const mixin = require('./lib/mixinHelper');
 const utils = require('./lib/utils');
-const writeToFile = lib.writeToFile;
 const conf = require('./lib/Config');
 
 var assetsDir = conf.get('assetsDir');
@@ -22,14 +21,27 @@ const fullMT = path.normalize(assetsDir + conf.get('storeDirName') + conf.get('m
 const storePath = path.normalize(assetsDir + conf.get('storeDirName'));
 const mixinsPath = path.normalize(assetsDir + conf.get('mixinsDirName'));
 const routerPath = path.normalize(assetsDir + conf.get('routerDirName') + '/index.js');
+const langDirPath = path.normalize(assetsDir + conf.get('langDirName'));
 
-const argv = require('yargs').argv;
+require('yargs')
+    .command(['route:list'], 'Show route list', {}, (argv) => {
+        dias.routeList(routerPath);
+    })
+    .command(['store:make <module_name> <module_structure>'], 'Make store', {}, (argv) => {
+        let stateFields = utils.parseStructure(argv.module_structure);
+        let storeFolderName = storePath + argv.module_name;
+        let mixinFilename = mixinsPath + argv.module_name + '.js';
+        //dias.extendMutationTypes(fullMT, utils.normalizeFields(stateFields));
+        dias.addStoreFile(stateFields, argv.module_name, storePath);
+    })
+    .command(['lang:fix'], 'Fix lang files', {}, (argv) => {
+        dias.fixLanguages(langDirPath);
+    })
+    .help()
+    .demandCommand()
+    .argv;
 
-var command = process.argv[2];
-if (argv._.indexOf('route:list') !== -1) {
-    dias.routeList(routerPath);
-}
-
+/*var command = process.argv[2];
 if (command == 'store') {
     var moduleName = process.argv[3];
     var moduleStructure = process.argv[4];
@@ -46,7 +58,6 @@ if (command == 'store') {
     let storeFilename = storePath + moduleName + '.js';
     let mixinFilename = mixinsPath + moduleName + 'Mixin.js';
 
-    dias.extendMutationTypes(fullMT, utils.normalizeFields(stateFields));
-    dias.addStoreFile(stateFields, moduleName, storePath);
+    
     dias.addMixinFile(utils.normalizeFields(stateFields), mixinFilename);
-}
+}*/
